f10e08a7a9d558237624932e3b2969d3f553e9cd
muhammaduzairrazaq <muhammaduzairrazaq@gmail.com>
2024-01-18 14:01:07 +0500

    solve profile image error

--- navigations/Navigation.js
+++ navigations/Navigation.js
@@ -1,4 +1,8 @@
-import { DarkTheme, DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import {
+  DarkTheme,
+  DefaultTheme,
+  NavigationContainer,
+} from "@react-navigation/native";
 import Feed from "./screens/tabScreens/Feed";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Settings from "./screens/tabScreens/Settings";
@@ -8,34 +12,35 @@ import { createStackNavigator } from "@react-navigation/stack";
 import TweetDetailsScreen from "./screens/homeStack/TweetDetailsScreen";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import Payments from "./screens/drawerScreens/Payments";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs"
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { useColorScheme } from "react-native";
 import { StatusBar } from "expo-status-bar";
- 
+import { Pressable, Image } from "react-native";
+
 const Tab = createBottomTabNavigator();
 const HomeStack = createStackNavigator();
 
 const TopTabs = createMaterialTopTabNavigator();
 function TopTabsGroup() {
-  return(
+  return (
     <TopTabs.Navigator
-    screenOptions={{
-      tabBarLabelStyle: {
-        textTransform: "capitalize",
-        fontWeight: "bold",
-      },
-      tabBarIndicatorStyle: {
-        height: 5,
-        borderRadius: 5,
-        backgroundColor: "#1DA1F2",
-      }
-    }}
+      screenOptions={{
+        tabBarLabelStyle: {
+          textTransform: "capitalize",
+          fontWeight: "bold",
+        },
+        tabBarIndicatorStyle: {
+          height: 5,
+          borderRadius: 5,
+          backgroundColor: "#1DA1F2",
+        },
+      }}
     >
       <TopTabs.Screen name="main" component={Feed} />
       <TopTabs.Screen name="Following" component={Payments} />
       <TopTabs.Screen name="Following2" component={Payments} />
     </TopTabs.Navigator>
-  )
+  );
 }
 
 function HomeStackGroup() {
@@ -55,7 +60,7 @@ function HomeStackGroup() {
   );
 }
 
-function TabGroup() {
+function TabGroup({ navigation }) {
   return (
     <Tab.Navigator
       screenOptions={({ route, navigation }) => ({
@@ -77,7 +82,25 @@ function TabGroup() {
       <Tab.Screen
         name="Feed"
         component={TopTabsGroup}
-        options={{ headerShown: true, tabBarLabel: "Home" }}
+        options={{
+          headerShown: true,
+          tabBarLabel: "Home",
+
+          headerLeft: () => (
+            <Pressable onPress={() => navigation.openDrawer()}>
+              <Image
+                source={require("./assets/beto.jpeg")}
+                style={{
+                  width: 40,
+                  height: 40,
+                  borderRadius: 100,
+                  marginLeft: 15,
+                }}
+              />
+            </Pressable>
+          ),
+          // headerRight: () => <Button title="Test" />,
+        }}
       />
       <Tab.Screen name="Notifications" component={Notifications} />
       <Tab.Screen name="Settings" component={Settings} />
@@ -91,18 +114,20 @@ function DrawerGroup() {
   return (
     <Drawer.Navigator screenOptions={{ headerShown: false }}>
       <Drawer.Screen name="HomeStackGroup" component={HomeStackGroup} />
-      <Drawer.Screen name="Payments" component={Payments} options={{ headerShown: true }} />
-    </Drawer.Navigator> 
+      <Drawer.Screen
+        name="Payments"
+        component={Payments}
+        options={{ headerShown: true }}
+      />
+    </Drawer.Navigator>
   );
 }
-
-
 
 export default function Navigation() {
   const currentTheme = useColorScheme();
   return (
     <NavigationContainer
-    theme={currentTheme === "dark"? DarkTheme : DefaultTheme} 
+      theme={currentTheme === "dark" ? DarkTheme : DefaultTheme}
     >
       <StatusBar style="auto" />
       <DrawerGroup />
